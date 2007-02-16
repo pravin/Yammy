b6985e7ee23fa91b6258cf74e7be63c81aa467fa
pravinp <pravinp@1caaf95c-cbde-4a29-ab83-04f0999a2351>
2007-02-16 16:00:21 +0000

    i18n: Webroot partially modified

--- trunk/src/Webroot/header.php
+++ trunk/src/Webroot/header.php
@@ -3,10 +3,19 @@
 	<title>Yammy 0.9: <$PageTitle$></title>
 	<link rel="stylesheet" type="text/css" href="display.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="print.css" media="print" />
+	<script src="<$LocaleString$>.js" type="text/javascript"></script>
 </head>
 <body>
 <div class="wrapper"><div class="container">
 	<div class="header">
+		<div class="search-box">
+			<form action="/search" method="post">
+				<input name="query" />
+				<script type="text/javascript">
+				document.write('<input type="submit" value="' + Search + '" />');
+				</script>
+			</form>
+		</div>
 		<h1><a href="http://yammy.sourceforge.net">Yammy 0.9</a></h1>Yahoo Messenger archives decoder
 	</div>
 	<div class="nav-bar">
@@ -14,5 +23,6 @@
 		<li><a href="/" title="Main Screen">Main</a></li>
 		<li><a href="/settings" title="Change Settings">Settings</a></li>
 		<li><a href="/help" title="Help">Help</a></li>
+		<li><a href="/log" title="View Log">View Log</a></li>
 	</ul>
 	</div>
