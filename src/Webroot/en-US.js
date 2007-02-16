8949c3f9457d4faa73be46f430eb6e2ccf624635
Pravin Paratey <pravin@paratey.com>
2007-02-16 16:00:21 +0000

    i18n: Webroot partially modified

---
+++ src/Webroot/en-US.js
@@ -0,0 +1,43 @@
+﻿/* Yammy 0.9 Language Resource File
+ * (http://yammy.sourceforge.net)
+ * 
+ * Note to Translators:
+ * - Please save this file in utf-8 format. To learn more about saving a file in utf-8, visit [TODO]
+ * - Do fill in the Language, Name and Contact details
+ *
+ * Language: English
+ * Translator: Pravin Paratey
+ * Contact: pravinp at gmail dot com
+ *
+ */
+
+var AppName = "Yammy"; // Do not edit
+var AppVersion = "0.9"; // Do not edit
+
+// Generic Strings
+var Error = "Error"; // Generic error string
+var More = "More";
+
+// Tray Menu Items
+// Note: Use & before the character to be underlined in the menu
+var TrayMenuOpen = "&Open";
+var TrayMenuSettings = "&Settings";
+var TrayMenuExit = "&Exit";
+
+var ConversationBetween = "Conversation between {0} and {1}";
+var ConversationStarted = "Conversation started on {0}";
+
+var NumSearchResults = "{0} of {1} search results";
+var PrevPage = "Previous Page";
+var NextPage = "Next Page";
+var ExportConvos = "Export Conversations";
+
+// HTML specific
+var FileNotFound = "File not found";
+var YahooPath = "Yahoo Path";
+var IndexPath = "Index Path";
+var UpdateFreq = "Update Frequency";
+var Language = "Language";
+var Hours = "hrs";
+var SaveSettings = "Save Settings";
+var Search = "Search";
