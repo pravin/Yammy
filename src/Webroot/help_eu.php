b72874a12136b7ebef81d1cc20e447eadf82215d
Pravin Paratey <pravin@paratey.com>
2008-02-15 07:59:21 +0000

    Adding help file for Basque language

---
+++ src/Webroot/help_eu.php
@@ -0,0 +1,48 @@
+<h1>Laguntza</h1>
+<h2>Gauza berriak</h2>
+<ol>
+	<li>Erabiltzaile interfaze berria eta intiuzio gehiagokoa.</li>
+	<li>Elkarrizketa guztien barnean bilatzeko gaitasuna.</li>
+	<li>Begitarteen erabilera.</li>
+	<li>Batazbesteko memoria gutxiago behar du. 20-30mbtik 3mb baino gutxiagora (0.8 betatik).</li>
+	<li>.NET 2.0 erabiltzen du</li>
+	<li>Inprimatzeko eta bistaratzeko estilo desberdinak erabiltzen ditu.</li>
+</ol> 
+
+<h2>FAQ</h2>
+<dl>
+<dt>1. Zergatik EI berria?</dt>
+	<dd><p><b>Lehenengo arrazoia:</b> Zerbait desberdina egiteko beharra nuen. <i>Web-UIa is in. Windows GUI is out.</i></p>
+	<p><b>Bigarren arrazoia:</b> Praktikoagoa da. Nire lanean <i>bakend stuf</i>a idazten. Programatzerakoan, GUIak baino liburutegi askoz gehiago egin ditut nire bizitzan zehar. Gainera, Windowsen programazioak, edo arazo honetarako, GUI programazioak ez du erraztasunik ematen logika eta aurkezpena bereizteko. Luzaroan ausnartu eta gero, zein izan da nire konponbidea? Hau.</p></dd>
+<dt>2. Zergatik erabili behar dut nire nabigatzailea? Nire datuak seguru al daude?</dt>
+	<dd><p>Ez zara internetera helduko. Lan guztia zure ordenagailuan egingo duzu. Yammy web serbitzari arin bat exekutatzen du. Horrela, datuak zure ordenagailuan egongo dira soilik denbora guztian zehar. Erabiltzaileak <b>ezin dute</b> beste ordenagailu batetik Yammyra heldu.</p>
+	<p>Eta nabigatzailea? Datuak erakuzteko erabili ohi da. Benetan paranoikoa baldin bazara, nabigatzailearen historia eta cachea ezabatu ditzakezu.</p></dd>
+<dt>3. ¿Beste edonorren kontua <i>hackeatu</i> dezaket?</dt>
+	<dd><p>Um, ez. Baina zure PCan biltegiratutako elkarrizketa guztiak ikusi ditzakezu. Horrela, Yammy erabili dezakezu (era batean) zure seme-alabak monitorizatzeko beraien elkarrizketen biltegiraketa aukera gaituz.</p>
+	<p>Dena den, zure saretik doazen elkarrizketak begiratzeko programa baten bila bazaude, begiratu honako web-orrian: <a href="http://imsniffer.sourceforge.net/" title="im sniffer">Im-Sniffer</a>.</p></dd>
+<dt>4. Yammy hasterakoan, 'The application failed to initialize properly' mezua jasotzen dut.</dt>
+	<dd><p><a href="http://www.microsoft.com/downloads/details.aspx?FamilyID=0856eacb-4362-4b0d-8edd-aab15c5e04f5&displaylang=en" title=".net 2.0 runtime">.NET 2.0 Runtime</a>-a deskargatu eta instalatu beharko duzu.</p></dd>
+<dt>5. Bilaketa saiatzerakoan, "No Index Found" errorea jasotzen dut.</dt>
+	<dd><p>Zure indizea ez da oraindik sortu. Saiatu berriro denbora batera. Oraindik mezua jasotzen baduzu, orduan arazo bat dago. Bisitatu <a href="http://yammy.sf.net/forums" title="visit forums">forumak</a> edo <a href="mailto:pravinp@gmail.com" title="email me">posta bat bidali</a>.</p></dd>
+<dt>6. Inprimatzerakoan, beste itxura dauka.</dt>
+	<dd><p>Inprimatzeko estilo desberdin bat erabili dut. Estilo horretan kolore irudimentsuak eta atzeko planoa desagertu egin dira. Edozein arrazoiagatik, inprimatu nahi baldin badituzu kolore irudimentsu horiek, kopiatu <code>display.css</code> fitxategiaren edukia <code>print.css</code>-ra.</p></dd>
+<dt>7. Zergatik urruneko erabiltzaile guztiak ikono berbera dute?</dt>
+	<dd><p>Oraindik ez dut urruneko erabiltzaileen ikonoak hartzeko era aurkitu. Horregatik oraingoz honako itxura edukiko dute:<br /><img src="/images/generic.png" width="48" height="48" /></p></dd>
+<dt>8. Ez naiz urruneko erabiltzailea, baina... nire ikonoak itxura berdina dute!</dt>
+	<dd><p>Hori zure irudia oraindik ez duzulako aldatu gertatzen da. Jarri beste irudi bat eta berriro saiatu.</p></dd>
+</dl>
+
+<p>Guzti hau ez bazaitu lagundu, <a href="http://yammy.sf.net/forums" title="visit forums">bisitatu gure forumak</a> eta utzi zure galderak.</p> 
+
+<h2>Eskerrak</h2>
+Proiektu bat ez da bakarrik programaketariekin osatzen. Badaude proiektuak lurrera jaizten dituzten eta lan gehiago egitera inspiratzen gaituzten erabiltzaileak. Proiektu hau desberdina egin duten pertsonei eskertu nahi diet,
+<dl>
+<dt>Andrei Virlan</dt>
+	<dd>Proiektu honi lehendabiziko bulkada emateagatik. Berak lagundu nau hizkuntza euskarriarekin.</dd>
+<dt>Laurentiu Nicola (GrayShade)</dt>
+	<dd>Beti hor egoteagatik. Eta nik bertan ez nenbilenean beste erabiltzaileek zituzten arazoak konpontzeagatik.</dd>
+<dt>HeyItsMe</dt>
+	<dd>Hurrengo bertsioa atera nuenerarte burua berotzeagatik. Ez nuke 0.7 bertsioa atera, beragatik ez balitz. Bere ideia argien eta hobekuntza eskaerengatik ere. Bera izan da dudarik gabe erabiltzaile langileena.</dd>
+</dl>
+
+<em>(Euskara itzulpena <a href="www.twolf.eu">Aitor Gomez Goiri</a>ren eskutik)</em>
